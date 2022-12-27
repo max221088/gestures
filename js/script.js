b82cd0a9761e9870f7076e7f9c9d707ba8094a10
max221088 <max.work221088@gmail.com>
2022-12-27 22:11:35 +0200

    add seved coords to local storage

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
 const $area = document.querySelector(".area");
-const $box = document.querySelector(".box");
 const $bottom = document.querySelector(".btn");
 
 const $areaWidth = window.innerWidth;
 //console.log(Width);
 const $areaHeight = $area.offsetHeight;
 //console.log(Height);
-const $boxWidth = $box.offsetWidth;
+// const $boxWidth = $box.offsetWidth;
+const $boxWidth = 100;
 //console.log($boxWidth);
-const $boxHeight = $box.offsetHeight;
+//const $boxHeight = $box.offsetHeight;
+const $boxHeight = 100;
 //console.log($boxWidth);
 
-let activeBox = "";
+let activeBox = null;
+let activeBoxIndex = null;
 let action = false;
 let startCoords = {
 	x: 0,
@@ -27,22 +29,47 @@ let distance = {
 	y: 0,
 };
 
+let boxes = [];
+
+if (localStorage.getItem("coords")) {
+	boxes = JSON.parse(localStorage.getItem("coords"));
+	boxGenerator(boxes);
+}
+
 function move(coords) {
 	activeBox.style.top = coords.y + "px";
 	activeBox.style.left = coords.x + "px";
 	//console.log(activeBox);
 }
 
+function boxGenerator(list) {
+	let template = "";
+	for (let i = 0; i < list.length; i++) {
+		template +=
+			'<div class="box" style="left: ' +
+			list[i].x +
+			"px; top: " +
+			list[i].y +
+			'px;" data-index="' +
+			i +
+			'">' +
+			(i + 1) +
+			"</div>";
+	}
+	$area.innerHTML = template;
+}
+
 $area.addEventListener("mousedown", function (e) {
 	//console.log(e);
 	if (e.target.classList.value == "box") {
 		activeBox = e.target;
+		activeBoxIndex = e.target.getAttribute("data-index");
 		//console.log(activeBox);
 		action = true;
 		startCoords.x = e.pageX;
 		startCoords.y = e.pageY;
-		saveCoords.y = e.target.getBoundingClientRect().top;
-		saveCoords.x = e.target.getBoundingClientRect().left;
+		saveCoords.y = boxes[activeBoxIndex].y;
+		saveCoords.x = boxes[activeBoxIndex].x;
 
 		//console.log(e.target.getBoundingClientRect().top);
 	}
@@ -54,13 +81,13 @@ $area.addEventListener("mouseup", function (e) {
 	//console.log(2);
 
 	action = false;
-	//saveCoords.y = distance.y;
-	//saveCoords.x = distance.x;
+	boxes[activeBoxIndex].x = distance.x;
+	boxes[activeBoxIndex].y = distance.y;
+	localStorage.setItem("coords", JSON.stringify(boxes));
 });
 
 $area.addEventListener("mousemove", function (e) {
 	//console.log(3);
-
 	if (action) {
 		distance.y = saveCoords.y + e.pageY - startCoords.y;
 		distance.x = saveCoords.x + e.pageX - startCoords.x;
@@ -92,7 +119,11 @@ $area.addEventListener("mousemove", function (e) {
 		}
 	}
 });
-
 $bottom.addEventListener("click", function () {
-	$area.insertAdjacentHTML("beforeend", '<div class="box"></div>');
+	boxes.push({
+		x: 0,
+		y: 0,
+	});
+	//console.log(boxes);
+	boxGenerator(boxes);
 });
